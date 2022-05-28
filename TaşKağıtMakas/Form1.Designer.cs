be1f8337b7d58dd157b5c8bd374c3e0c166da88a
mKosemen <103363649+mKosemen@users.noreply.github.com>
2022-05-28 19:19:44 +0300

    Add project files.

---
+++ TaşKağıtMakas/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace TaşKağıtMakas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnRTas = new System.Windows.Forms.Button();
+            this.btnRKagit = new System.Windows.Forms.Button();
+            this.btnRMakas = new System.Windows.Forms.Button();
+            this.btnBTas = new System.Windows.Forms.Button();
+            this.btnBKagit = new System.Windows.Forms.Button();
+            this.btnBMakas = new System.Windows.Forms.Button();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.lblRSkor = new System.Windows.Forms.Label();
+            this.lblBSkor = new System.Windows.Forms.Label();
+            this.lblRTur = new System.Windows.Forms.Label();
+            this.lblBTur = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRTas
+            // 
+            this.btnRTas.BackgroundImage = global::TaşKağıtMakas.Properties.Resources.Taş;
+            this.btnRTas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnRTas.Enabled = false;
+            this.btnRTas.Location = new System.Drawing.Point(82, 120);
+            this.btnRTas.Name = "btnRTas";
+            this.btnRTas.Size = new System.Drawing.Size(88, 42);
+            this.btnRTas.TabIndex = 0;
+            this.btnRTas.UseVisualStyleBackColor = true;
+            // 
+            // btnRKagit
+            // 
+            this.btnRKagit.BackgroundImage = global::TaşKağıtMakas.Properties.Resources.Kağıt;
+            this.btnRKagit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnRKagit.Enabled = false;
+            this.btnRKagit.Location = new System.Drawing.Point(176, 120);
+            this.btnRKagit.Name = "btnRKagit";
+            this.btnRKagit.Size = new System.Drawing.Size(88, 42);
+            this.btnRKagit.TabIndex = 1;
+            this.btnRKagit.UseVisualStyleBackColor = true;
+            // 
+            // btnRMakas
+            // 
+            this.btnRMakas.BackgroundImage = global::TaşKağıtMakas.Properties.Resources.Makas;
+            this.btnRMakas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnRMakas.Enabled = false;
+            this.btnRMakas.Location = new System.Drawing.Point(268, 120);
+            this.btnRMakas.Name = "btnRMakas";
+            this.btnRMakas.Size = new System.Drawing.Size(88, 42);
+            this.btnRMakas.TabIndex = 2;
+            this.btnRMakas.UseVisualStyleBackColor = true;
+            // 
+            // btnBTas
+            // 
+            this.btnBTas.BackgroundImage = global::TaşKağıtMakas.Properties.Resources.Taş;
+            this.btnBTas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnBTas.Location = new System.Drawing.Point(82, 254);
+            this.btnBTas.Name = "btnBTas";
+            this.btnBTas.Size = new System.Drawing.Size(88, 42);
+            this.btnBTas.TabIndex = 3;
+            this.btnBTas.UseVisualStyleBackColor = true;
+            this.btnBTas.Click += new System.EventHandler(this.btnBTas_Click);
+            // 
+            // btnBKagit
+            // 
+            this.btnBKagit.BackgroundImage = global::TaşKağıtMakas.Properties.Resources.Kağıt;
+            this.btnBKagit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnBKagit.Location = new System.Drawing.Point(176, 254);
+            this.btnBKagit.Name = "btnBKagit";
+            this.btnBKagit.Size = new System.Drawing.Size(88, 42);
+            this.btnBKagit.TabIndex = 4;
+            this.btnBKagit.UseVisualStyleBackColor = true;
+            this.btnBKagit.Click += new System.EventHandler(this.btnBKagit_Click);
+            // 
+            // btnBMakas
+            // 
+            this.btnBMakas.BackgroundImage = global::TaşKağıtMakas.Properties.Resources.Makas;
+            this.btnBMakas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnBMakas.Location = new System.Drawing.Point(270, 254);
+            this.btnBMakas.Name = "btnBMakas";
+            this.btnBMakas.Size = new System.Drawing.Size(86, 42);
+            this.btnBMakas.TabIndex = 5;
+            this.btnBMakas.UseVisualStyleBackColor = true;
+            this.btnBMakas.Click += new System.EventHandler(this.btnBMakas_Click_1);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.BackColor = System.Drawing.Color.Transparent;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.ForeColor = System.Drawing.Color.Black;
+            this.lblSonuc.Location = new System.Drawing.Point(22, 201);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(0, 24);
+            this.lblSonuc.TabIndex = 6;
+            this.lblSonuc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRSkor
+            // 
+            this.lblRSkor.AutoSize = true;
+            this.lblRSkor.BackColor = System.Drawing.Color.Transparent;
+            this.lblRSkor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRSkor.ForeColor = System.Drawing.Color.DimGray;
+            this.lblRSkor.Location = new System.Drawing.Point(208, 69);
+            this.lblRSkor.Name = "lblRSkor";
+            this.lblRSkor.Size = new System.Drawing.Size(25, 25);
+            this.lblRSkor.TabIndex = 7;
+            this.lblRSkor.Text = "0";
+            // 
+            // lblBSkor
+            // 
+            this.lblBSkor.AutoSize = true;
+            this.lblBSkor.BackColor = System.Drawing.Color.Transparent;
+            this.lblBSkor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBSkor.ForeColor = System.Drawing.Color.DimGray;
+            this.lblBSkor.Location = new System.Drawing.Point(208, 323);
+            this.lblBSkor.Name = "lblBSkor";
+            this.lblBSkor.Size = new System.Drawing.Size(25, 25);
+            this.lblBSkor.TabIndex = 8;
+            this.lblBSkor.Text = "0";
+            // 
+            // lblRTur
+            // 
+            this.lblRTur.AutoSize = true;
+            this.lblRTur.BackColor = System.Drawing.Color.Transparent;
+            this.lblRTur.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRTur.Location = new System.Drawing.Point(12, 135);
+            this.lblRTur.Name = "lblRTur";
+            this.lblRTur.Size = new System.Drawing.Size(21, 24);
+            this.lblRTur.TabIndex = 9;
+            this.lblRTur.Text = "0";
+            // 
+            // lblBTur
+            // 
+            this.lblBTur.AutoSize = true;
+            this.lblBTur.BackColor = System.Drawing.Color.Transparent;
+            this.lblBTur.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBTur.Location = new System.Drawing.Point(12, 269);
+            this.lblBTur.Name = "lblBTur";
+            this.lblBTur.Size = new System.Drawing.Size(21, 24);
+            this.lblBTur.TabIndex = 10;
+            this.lblBTur.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::TaşKağıtMakas.Properties.Resources.yenile;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 34);
+            this.button1.TabIndex = 11;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TaşKağıtMakas.Properties.Resources.Fon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(408, 378);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblBTur);
+            this.Controls.Add(this.lblRTur);
+            this.Controls.Add(this.lblBSkor);
+            this.Controls.Add(this.lblRSkor);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btnBMakas);
+            this.Controls.Add(this.btnBKagit);
+            this.Controls.Add(this.btnBTas);
+            this.Controls.Add(this.btnRMakas);
+            this.Controls.Add(this.btnRKagit);
+            this.Controls.Add(this.btnRTas);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRTas;
+        private System.Windows.Forms.Button btnRKagit;
+        private System.Windows.Forms.Button btnRMakas;
+        private System.Windows.Forms.Button btnBTas;
+        private System.Windows.Forms.Button btnBKagit;
+        private System.Windows.Forms.Button btnBMakas;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Label lblRSkor;
+        private System.Windows.Forms.Label lblBSkor;
+        private System.Windows.Forms.Label lblRTur;
+        private System.Windows.Forms.Label lblBTur;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TaşKağıtMakas/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaşKağıtMakas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string oSecimi, bSecimi;
+        int oPuan, bPuan,oTur,bTur;
+        int rastgele;
+        Random random = new Random();
+        private void btnBTas_Click(object sender, EventArgs e)
+        {
+            ButonlarıBeyazaCevir();
+            oSecimi = "Taş";
+            Bilgisayar_Hamle();
+            puanlama();
+            btnBTas.BackColor = Color.Yellow;
+        }
+        private void btnBKagit_Click(object sender, EventArgs e)
+        {
+            ButonlarıBeyazaCevir();
+            oSecimi = "Kağıt";
+            Bilgisayar_Hamle();
+            puanlama();
+            btnBKagit.BackColor = Color.Yellow;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnBKagit.Enabled = true;
+            btnBTas.Enabled = true;
+            btnBMakas.Enabled = true;
+            oTur = 0;
+            bTur = 0;
+            oPuan = 0;
+            bPuan = 0;
+            lblBTur.Text = oTur.ToString();
+            lblRTur.Text = bTur.ToString();
+            lblBSkor.Text = oPuan.ToString();
+            lblRSkor.Text = bPuan.ToString();
+        }
+
+        private void btnBMakas_Click_1(object sender, EventArgs e)
+        {
+            ButonlarıBeyazaCevir();
+            oSecimi = "Makas";
+            Bilgisayar_Hamle();
+            puanlama();
+            btnBMakas.BackColor = Color.Yellow;
+        }
+        private void ButonlarıBeyazaCevir()
+        {
+            btnBKagit.BackColor = Color.White;
+            btnBTas.BackColor = Color.White;
+            btnBMakas.BackColor = Color.White;
+            btnRKagit.BackColor = Color.White;
+            btnRTas.BackColor = Color.White;
+            btnRMakas.BackColor = Color.White;
+        }
+        private void Bilgisayar_Hamle()
+        {
+            rastgele = random.Next(1, 4);
+            if (rastgele == 1)
+                bSecimi = "Taş";
+            else if (rastgele == 2)
+                bSecimi = "Kağıt";
+            else if (rastgele == 3)
+                bSecimi = "Makas";
+        }
+        private void puanlama()
+        {
+            if (oSecimi == "Taş" && bSecimi == "Kağıt")
+            {
+                btnRKagit.BackColor = Color.Red;
+                bPuan++;
+                lblRSkor.Text = bPuan.ToString();
+                lblSonuc.Text = "KAYBETTİN";
+
+            }
+            else if (oSecimi == "Taş" && bSecimi == "Makas")
+            {
+                btnRMakas.BackColor = Color.Green;
+                oPuan++;
+                lblBSkor.Text = oPuan.ToString();
+                lblSonuc.Text = "KAZANDIN";
+            }
+            else if (oSecimi == "Taş" && bSecimi == "Taş")
+            {
+                btnRTas.BackColor = Color.Yellow;
+                lblSonuc.Text = "BERABERE";
+            }
+            else if (oSecimi == "Kağıt" && bSecimi == "Taş")
+            {
+                btnRTas.BackColor = Color.Green;
+                oPuan++;
+                lblBSkor.Text = oPuan.ToString();
+                lblSonuc.Text = "KAZANDIN";
+            }
+            else if (oSecimi == "Kağıt" && bSecimi == "Makas")
+            {
+                btnRMakas.BackColor = Color.Red;
+                bPuan++;
+                lblRSkor.Text = bPuan.ToString();
+                lblSonuc.Text = "KAYBETTİN";
+            }
+            else if (oSecimi == "Kağıt" && bSecimi == "Kağıt")
+            {
+                btnRKagit.BackColor = Color.Yellow;
+                lblSonuc.Text = "BERABERE";
+            }
+            else if (oSecimi == "Makas" && bSecimi == "Taş")
+            {
+                btnRTas.BackColor = Color.Red;
+                bPuan++;
+                lblRSkor.Text = bPuan.ToString();
+                lblSonuc.Text = "KAYBETTİN";
+            }
+            else if (oSecimi == "Makas" && bSecimi == "Kağıt")
+            {
+                btnRKagit.BackColor = Color.Green;
+                oPuan++;
+                lblBSkor.Text = oPuan.ToString();
+                lblSonuc.Text = "KAZANDIN";
+            }
+            else if (oSecimi == "Makas" && bSecimi == "Makas")
+            {
+                btnRMakas.BackColor = Color.Yellow;
+                lblSonuc.Text = "BERABERE";
+            }
+            if (bPuan == 5 || oPuan == 5)
+            {
+                if (bPuan > oPuan)
+                {
+                    lblSonuc.Text = "TURU BİLGİSAYAR KAZANDI";
+                    oPuan = 0;
+                    bPuan = 0;
+                    bSecimi = null;
+                    oSecimi = null;
+                    bTur++;
+                    lblRTur.Text = bTur.ToString();
+                    lblBSkor.Text = oPuan.ToString();
+                    lblRSkor.Text = bPuan.ToString();
+                }
+                else if (oPuan > bPuan)
+                {
+                    lblSonuc.Text = "TURU SİZ KAZANDINIZ";
+                    oPuan = 0;
+                    bPuan = 0;
+                    bSecimi = null;
+                    oSecimi = null;
+                    oTur++;
+                    lblBTur.Text = oTur.ToString();
+                    lblBSkor.Text = oPuan.ToString();
+                    lblRSkor.Text = bPuan.ToString();
+                }
+                if ((oTur == 5 || bTur == 5))
+                {
+                    if (oTur> bTur)
+                    {
+                        lblSonuc.Text = "OYUNU KAZANDINIZ";
+                        btnBKagit.Enabled = false;
+                        btnBTas.Enabled = false;
+                        btnBMakas.Enabled = false;
+                    }
+                    if (oTur < bTur)
+                    {
+                        lblSonuc.Text = "OYUNU KAYBETTİNİZ";
+                        btnBKagit.Enabled = false;
+                        btnBTas.Enabled = false;
+                        btnBMakas.Enabled = false;
+                    }
+                }
+            }
+        }
+    }
+}
